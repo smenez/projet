a06701820c811b80850e89ecb6f933165dc7b3a4
Sasha <smenez@gaming.tech>
2024-01-15 13:36:46 +0100

    update

--- includes/header.php
+++ includes/header.php
@@ -26,16 +26,20 @@
                     ;
                 }
                 ?>
-                <div class="cache">
-                    <li><a href="https://store.steampowered.com/app/1044720/Farthest_Frontier/" target="_blank">Steam</a></li>
-                    <li><a href="https://www.youtube.com/c/crateentertainment" target="_blank">Youtube</a></li>
-                    <li><a href="https://www.twitch.tv/crateentertainment" target="_blank">Twitch</a></li>
-                    <li><a href="https://twitter.com/crate_frontier" target="_blank">Twitter</a></li>
-                    <li><a href="info.php">Infos</a></li>
-                    <li><a href="forum.php">Forum</a></li>
-                    <li><a href="announcement.php">Announcement</a></li>
-                    <li><a href="user.php">Profil</a></li>
-                </div>
+                <section class="cache">
+                    <div class="flyout-1">
+                        <li class="flyout-1-1"><a href="https://store.steampowered.com/app/1044720/Farthest_Frontier/" target="_blank">Steam</a></li>
+                        <li class="flyout-1-1"><a href="https://www.youtube.com/c/crateentertainment" target="_blank">Youtube</a></li>
+                        <li class="flyout-1-2"><a href="https://www.twitch.tv/crateentertainment" target="_blank">Twitch</a></li>
+                        <li class="flyout-1-2"><a href="https://twitter.com/crate_frontier" target="_blank">Twitter</a></li>
+                    </div>
+                    <div class="flyout-2">
+                        <li class="flyout-2-1"><a href="info.php">Infos</a></li>
+                        <li class="flyout-2-1"><a href="forum.php">Forum</a></li>
+                        <li class="flyout-2-2"><a href="announcement.php">Announcement</a></li>
+                        <li class="flyout-2-2"><a href="user.php">Profil</a></li>
+                    </div>
+            </section>
         </ul>
     </nav>
     <button><img src="https://cdn-icons-png.flaticon.com/512/10080/10080458.png" alt="Menu déroulant"></button>
